4f3115263b01ade57937216c197df503038ada4b
NiButera <nikolasxp683@gmail.com>
2023-03-19 02:33:51 -0300

    Fiz o programa que exibe os nomes das cidades do Litoral Sul de SP em cada linha

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine();
+Console.WriteLine("Cidades do Litoral-Sul de São Paulo");
+
+Console.WriteLine();
+Console.WriteLine(@"Santos
+São Vicente
+Praia Grande
+Guarujá
+Cubatão
+Cargaquatuba
+Itanhaém
+Ubatuba
+São Sebastião
+Peruíbe
+Bertioga
+Mongaguá
+Ilhabela
+Iguape
+Cananéia
+Ilha Comprida");
+
+Console.WriteLine();
